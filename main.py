2f24d3e5497636e0eb44d17a1fe2e5af3eadde7f
Nash <67402799+park900720000@users.noreply.github.com>
2023-10-15 23:29:37 +0900

    Add files via upload
    update: 버튼 기능 개선

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
-# from dotenv import load_dotenv
-# load_dotenv()
+from dotenv import load_dotenv
+load_dotenv()
+
 import streamlit as st
 from langchain.chat_models import ChatOpenAI
 
 chat_model = ChatOpenAI()
 
-st.title('AI 시인')
+st.set_page_config(page_title="AI 시인 📖", layout="wide")
+st.title('AI 시인 📖')
 
-content = st.text_input('시의 주제를 제시해주세요.')
+content = st.text_input('시의 주제를 제시해주세요. 시가 작성된 이후 새로운 제시어를 입력하고 Enter를 입력하여 새로운 시를 작성할 수 있습니다.')
 
-if st.button('시 쓰기!'):
-        with st.spinner('시를 작성 중입니다..'):
-            result = chat_model.predict(content + "에 대한 시를 써주세요.")
-            st.write(result)
+class SessionState:
+    def __init__(self):
+        self.button_clicked = False
+
+try:
+    state = st.session_state["state"]
+except KeyError:
+    state = SessionState()
+    st.session_state["state"] = state
+
+if not state.button_clicked:
+    if st.button('시 작성하기'):
+        state.button_clicked = True
+else:
+    with st.spinner('시를 작성 중입니다..'):
+        result = chat_model.predict(content + "에 대한 시를 써주세요.")
+        st.write(result)
+        state.button_clicked = False
